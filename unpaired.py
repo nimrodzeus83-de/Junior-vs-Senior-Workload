6503dd4646158053d213b6ad165882e5cdc047d2
nimrodzeus83-de <nimrodzeus83@gmail.com>
2026-06-13 18:10:21 +0800

    Enhance statistical output and plot aesthetics
    Added mean, standard deviation, and variance calculations for Junior and Senior workload groups. Enhanced plot titles and labels with font weight and size.

--- unpaired.py
+++ unpaired.py
@@ -16,7 +16,12 @@ t_critical = stats.t.ppf(1-alpha / 2, degree_freedom)
 #print(junior_level.head(602))
 #print(senior_level.head(1069))
 t_statistics, p_value = stats.ttest_ind(junior_level, senior_level)
+mean1, mean2 = junior_level.mean(), senior_level.mean()
+sd1, sd2 = junior_level.std(), senior_level.std()
+var1, var2 = junior_level.var(), senior_level.var()
 
+print(f"Group 1 - Mean: {mean1}, Standard Deviation: {sd1}, Variance: {var1}")
+print(f"Group 2 - Mean: {mean2}, Standard Deviation: {sd2}, Variance: {var2}")
 print(f"Degree of Freedom: {degree_freedom}")
 print(f"T-Critical value: {t_critical}")
 print(f"T-Stat: {t_statistics}")
@@ -24,20 +29,25 @@ print(f"P-Value: {p_value}")
 print("")
 
 if p_value < alpha:
-    print("Reject H0")
     print("There is a significant difference between the workload of Junior and Senior Employees")
     
 else:
-  print("Failed to reject H0")
   print("There is no significant difference between the workload of Junior and Senior Employees")
+
 
 sns.kdeplot(junior_level, label="Junior Level", fill=True)
 sns.kdeplot(senior_level, label="Senior Level", fill=True)
-plt.title(f"Distribution between Job level and Work Load of Employees")
+plt.title(f"Distribution between Job level and Work Load of Employees",
+          fontsize=15,
+          fontweight="bold")
+
 plt.tight_layout()
-plt.xlabel("Workload")
-plt.ylabel("Frequency")
+
+plt.xlabel("Workload",fontsize=15,fontweight="bold")
+plt.ylabel("Frequency",fontsize=15,fontweight="bold")
+
 plt.legend()
 plt.savefig("unpaired_analysis.png")
 plt.show(block=True)
+
 
